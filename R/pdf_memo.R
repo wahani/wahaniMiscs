74fea6c0183ba60d35af6c8daaf604a87658a15e
Sebastian <wahani@gmail.com>
2015-01-08 15:24:09 +0100

    update in pdf_memo

--- R/pdf_memo.R
+++ R/pdf_memo.R
@@ -1,9 +1,10 @@
-pdf_memo <- function() {
+pdf_memo <- function(...) {
   template <- find_resource("wMemo", "template.tex")
   rmarkdown::pdf_document(
     template = template,
     keep_tex = FALSE,
     fig_width = 6.5,
-    fig_height = 3.5
+    fig_height = 3.5,
+    ...
   )
 }
