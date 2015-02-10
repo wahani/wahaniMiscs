a245ba8edf745357ff810b429741e9e070bd9977
Sebastian <wahani@gmail.com>
2015-02-10 11:14:38 +0100

    getLocalPackageUpdates

---
+++ R/getLocalPackageUpdates.R
@@ -0,0 +1,14 @@
+#' Function to report differences in NEWS file between CRAN Verion and local version of a package.
+#'
+#' @param package as character
+#' @export
+getLocalPackageUpdates <- function(package) {
+  ps <- summary(packageStatus())
+  cranVersion <- ps$avail[rownames(ps$avail) == package, "Version"]
+  localVersion <- ps$inst[rownames(ps$inst) == package, "Version"]
+  cat("Version on CRAN:", cranVersion, "\n")
+  cat("Development Version:", localVersion, "\n\n")
+  cat("Updates in package NEWS-file since last release to CRAN:\n\n")
+  print(news(Version > cranVersion, package = package))
+  invisible(NULL)
+}
